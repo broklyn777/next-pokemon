048d5bce67e83f8720a4afbc80910a7d54bb5020
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-04-28 23:31:15 +0200

    ssr take 2

--- pages/index.js
+++ pages/index.js
@@ -41,36 +41,3 @@ export default function Home({ pokemon }) {
     </div>
   );
 }
-
-
-
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
-// import React, { useState, useEffect } from "react";
-
-
-// export default function Home() {
-//   const [pokemon, setPokemon] = useState([]);
-
-
-//   useEffect(() => {
-//     async function getPokemon() {
-//       const resp = await fetch("https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json")
-//       setPokemon(await resp.json());
-//     }
-//     getPokemon();
-//   }, [])
-
-
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Pokemon</title>
-
-//       </Head>
-//       <div>
-//         {JSON.stringify(pokemon)}
-//       </div>
-//     </div>
-//   );
-// }
--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,74 +1,76 @@
 /* eslint-disable @next/next/no-img-element */
-import { useRouter } from "next/router"
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../../styles/Home.module.css";
+import styles from "../../styles/Details.module.css";
 
+export async function getStaticPaths() {
+    const resp = await fetch(
+        "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+    );
+    const pokemon = await resp.json();
 
+    return {
+        paths: pokemon.map((pokemon) => ({
+            params: { id: pokemon.id.toString() },
+        })),
+        fallback: false,
+    };
+}
 
-export default function Details() {
-    const {
-        query: { id },
-    } = useRouter()
-    const [pokemon, setPokemon] = useState(null)
+export async function getStaticProps({ params }) {
+    const resp = await fetch(
+        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+    );
 
+    return {
+        props: {
+            pokemon: await resp.json(),
+        },
+        // revalidate: 30,
+    };
+}
 
-    useEffect(() => {
-        async function getPokemon() {
-            const resp = await fetch(`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`)
-            setPokemon(await resp.json());
-        }
-        if (id) {
-            getPokemon()
-        }
-        getPokemon();
-    }, [id])
-
-    if (!pokemon) {
-        return null;
-    }
-
-
-    return (<div> <Head>
-        <title>{pokemon.name}</title>
-    </Head>
+export default function Details({ pokemon }) {
+    return (
         <div>
-            <Link href="/">
-                <a>Back to Home</a>
-            </Link>
-        </div>
-        <div className={styles.layout}>
+            <Head>
+                <title>{pokemon.name}</title>
+            </Head>
             <div>
-                <img
-                    className={styles.picture}
-                    src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
-                    alt={pokemon.name.english}
-                />
+                <Link href="/">
+                    <a>Back to Home</a>
+                </Link>
             </div>
-            <div>
-                <div className={styles.name}>{pokemon.name}</div>
-                <div className={styles.type}>{pokemon.type.join(", ")}</div>
-                <table>
-                    <thead className={styles.header}>
-                        <tr>
-                            <th>Name</th>
-                            <th>Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {pokemon.stats.map(({ name, value }) => (
-                            <tr key={name}>
-                                <td className={styles.attribute}>{name}</td>
-                                <td>{value}</td>
+            <div className={styles.layout}>
+                <div>
+                    <img
+                        className={styles.picture}
+                        src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                        alt={pokemon.name.english}
+                    />
+                </div>
+                <div>
+                    <div className={styles.name}>{pokemon.name}</div>
+                    <div className={styles.type}>{pokemon.type.join(", ")}</div>
+                    <table>
+                        <thead className={styles.header}>
+                            <tr>
+                                <th>Name</th>
+                                <th>Value</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {pokemon.stats.map(({ name, value }) => (
+                                <tr key={name}>
+                                    <td className={styles.attribute}>{name}</td>
+                                    <td>{value}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-
-    </div>
-
-    )
+    );
 }
